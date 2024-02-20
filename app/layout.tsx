3b346e6b87afcb651b8bc261b3ffa1781f9bf0cc
Roman Klimov <roman.klimov@bahnhof.net>
2024-02-20 22:07:34 +0100

    add Clerk auth

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from 'next';
 import { Poppins } from 'next/font/google';
+import { ClerkProvider } from '@clerk/nextjs';
+
 import './globals.css';
+
 
 const poppins = Poppins({
   subsets: [ 'latin' ],
@@ -22,8 +25,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-    <body className={poppins.variable}>{children}</body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+      <body className={poppins.variable}>{children}</body>
+      </html>
+    </ClerkProvider>
   );
 }
