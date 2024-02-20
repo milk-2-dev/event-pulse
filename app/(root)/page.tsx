558e4a0ec630af502614d54087f50ed3a67d5c5f
Roman Klimov <roman.klimov@bahnhof.net>
2024-02-20 21:01:14 +0100

    setup font & metadata

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,7 @@
-import { Button } from '@/components/ui/button';
-
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Button variant={'destructive'} className="px-10">Click me</Button>
+      <h1 className="text-4xl">Event pulse</h1>
     </main>
   );
 }
