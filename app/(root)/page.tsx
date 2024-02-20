74fea61b0ce9467bf67c5e926173b2b45cfd8035
Roman Klimov <roman.klimov@bahnhof.net>
2024-02-20 22:08:18 +0100

    some tsx minor changing

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,5 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-4xl">Event pulse</h1>
-    </main>
+    <h1 className="text-4xl">Event pulse</h1>
   );
 }
