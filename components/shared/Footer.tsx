6016afc8026a4e0028be2f93a11ad69454a63224
Roman Klimov <roman.klimov@bahnhof.net>
2024-02-21 14:38:33 +0100

    add footer

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -1,8 +1,24 @@
+import Link from 'next/link';
+import Image from 'next/image';
+
+
 const Footer = () => {
+  const currentYear = new Date().getFullYear();
+
   return (
-    <div>
-      Footer
-    </div>
+    <footer className="border-t">
+      <div className="flex-center wrapper flex-between flex flex-col gap-4 p-5 text-center sm:flex-row">
+        <Link href="/">
+          <Image
+            src="/assets/images/logo.svg"
+            width={128}
+            height={38}
+          />
+        </Link>
+
+        <p>{currentYear} Event pulse. All rights reserved.</p>
+      </div>
+    </footer>
   );
 };
 
