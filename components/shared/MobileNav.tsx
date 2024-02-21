49d219c609facbbc1e33eb0919264563b7ff3dee
Roman Klimov <roman.klimov@bahnhof.net>
2024-02-21 14:30:20 +0100

    add navs

--- components/shared/MobileNav.tsx
+++ components/shared/MobileNav.tsx
@@ -1,8 +1,45 @@
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from '@/components/ui/sheet';
+import { Separator } from '@/components/ui/separator';
+
+import Image from 'next/image';
+import NavItems from '@/components/shared/NavItems';
+
+
 const MobileNav = () => {
   return (
-    <div>
-      Mobile nav
-    </div>
+    <nav className="md:hidden">
+      <Sheet>
+        <SheetTrigger className="align-middle">
+          <Image
+            src="/assets/icons/menu.svg"
+            alt="Menu"
+            width={24}
+            height={24}
+            className="cursor-pointer"
+          />
+        </SheetTrigger>
+        <SheetContent className="flex flex-col gap-6 bg-white md:hidden">
+          <Image
+            src="/assets/images/logo.svg"
+            alt="Logo"
+            width={128}
+            height={38}
+          />
+
+          <Separator className="border border-gray-50"/>
+          <NavItems/>
+
+        </SheetContent>
+      </Sheet>
+
+    </nav>
   );
 };
 
