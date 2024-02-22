51ca097c4fbbe1b41c554a85eb6391398a599649
Roman Klimov <roman.klimov@bahnhof.net>
2024-02-22 23:43:52 +0100

    fix layout

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,4 +1,4 @@
-const Layout = ({children}: { childre: React.ReactNode }) => {
+const Layout = ({children}: { children: React.ReactNode }) => {
   return (
     <div className="flex-center min-h-screen w-full bg-primary-50 bg-dotted-pattern bg-cover bg-fixed bg-center">
       {children}
