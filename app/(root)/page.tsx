a2e0f2d8ba08bff0ffd8187b7162f66dc0fc461c
Roman Klimov <roman.klimov@bahnhof.net>
2024-02-25 15:20:36 +0100

    code style

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,7 @@
-import { Button } from '@/components/ui/button';
 import Link from 'next/link';
 import Image from 'next/image';
+
+import { Button } from '@/components/ui/button';
 
 
 export default function Home() {
@@ -32,13 +33,15 @@ export default function Home() {
       </section>
 
       <section id="events" className="wrapper my-8 flex flex-col gap-8 md:gap-12">
-        <h2 className="h2-bold">Trust by <br /> Thousands of Events</h2>
+        <h2 className="h2-bold">Trust by <br/> Thousands of Events</h2>
 
         <div className="flex w-full flex-col gap-5 md:flex-row">
+
           Search
           CategoryFilter
           {/*<Search />*/}
           {/*<CategoryFilter />*/}
+
         </div>
 
         {/*<Collection*/}
